584007e1944f4bf107e64bd86f092d62e5488f87
Austin Moody <amoody25@uncc.edu>
2020-10-02 15:30:26 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -2,25 +2,28 @@ const { kMaxLength } = require('buffer');
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const fs = require('fs');
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-    ]
-};
+let rawData =  fs.readFileSync('myBudget.json');
+let budget = JSON.parse(rawData);
+
+// const budget = {
+//     myBudget: [
+//     {
+//         title: 'Eat out',
+//         budget: 25
+//     },
+//     {
+//         title: 'Rent',
+//         budget: 375
+//     },
+//     {
+//         title: 'Grocery',
+//         budget: 110
+//     },
+//     ]
+// };
 
 app.get('/hello', (req,res) => {
     res.send('Hello World');
