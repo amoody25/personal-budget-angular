4b8dba972e2dabcbadbe522324b3df806b779a75
Austin Moody <amoody25@uncc.edu>
2020-10-06 15:16:59 -0400

    Added CORS to my Server.

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
-const { kMaxLength } = require('buffer');
+//Budget API
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
-const fs = require('fs');
-app.use('/', express.static('public'));
 
-let rawData =  fs.readFileSync('myBudget.json');
-let budget = JSON.parse(rawData);
+app.use(cors());
 
 // const budget = {
-//     myBudget: [
+//   myBudget: [
 //     {
-//         title: 'Eat out',
-//         budget: 25
+//        title: 'Eat out',
+//        budget: 25
 //     },
 //     {
-//         title: 'Rent',
-//         budget: 375
+//        title: 'Rent',
+//        budget: 375
 //     },
 //     {
-//         title: 'Grocery',
-//         budget: 110
+//        title: 'Grocery',
+//        budget: 110
 //     },
 //     ]
 // };
 
-app.get('/hello', (req,res) => {
-    res.send('Hello World');
-});
-
 app.get('/budget', (req,res) => {
-    res.json(budget);
+    res.sendFile('budget.json', {root: __dirname});
 });
 
 app.listen(port, () => {
-    console.log('Example app listening att http://localhost:${port}');
+    console.log(`API served at http://localhost:${port}`);
 });
